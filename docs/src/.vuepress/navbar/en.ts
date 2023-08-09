0a3ae78d236f7f13c65f85a68723ba2138c2761e
wangshiyu <wangshiyu@safeis.cn>
2023-08-09 14:08:36 +0800

    feat: navbar

--- docs/src/.vuepress/navbar/en.ts
+++ docs/src/.vuepress/navbar/en.ts
@@ -2,7 +2,33 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   "/",
-  { text: "Demo", icon: "discover", link: "/demo/" },
+  { text: "编程", icon: "discover", link: "/demo/" },
+  { text: "开源项目", icon: "discover", link: "/demo/" },
+  { text: "技术书籍", icon: "discover", link: "/demo/" },
+  { text: "知识星球", icon: "discover", link: "/demo/" },
+  { text: "AI", icon: "discover", link: "/demo/" },
+  { text: "区块链", icon: "discover", link: "/demo/" },
+  { text: "副业", icon: "discover", link: "/demo/" },
+  { text: "关于网站", icon: "discover", link: "/demo/" },
+  {
+    text: "关于网站",
+    icon: "creative",
+    prefix: "/guide/",
+    children: [
+      {
+        text: "作者",
+        icon: "creative",
+        prefix: "bar/",
+        children: ["baz", { text: "...", icon: "more", link: "" }],
+      },
+      {
+        text: "更新历史",
+        icon: "config",
+        prefix: "foo/",
+        children: ["ray", { text: "...", icon: "more", link: "" }],
+      },
+    ],
+  },
   {
     text: "Guide",
     icon: "creative",
