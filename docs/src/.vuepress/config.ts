047f9361096fbdd507279be029d950ffe106cbb7
rodert <731444260@qq.com>
2023-09-24 23:22:41 +0800

    u

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: JavaPub
+ * @Date: 2023-08-11 23:35:12
+ * @LastEditors: your name
+ * @LastEditTime: 2023-09-24 23:22:11
+ * @Description: Here is the JavaPub code base. Search JavaPub on the whole web.
+ * @FilePath: \JavaPub-temp\docs\src\.vuepress\config.ts
+ */
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -7,7 +15,7 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "en-US",
-      title: "JavaPub",
+      title: "JavaPub（面试学习一条龙）",
       description: "一个面试学习一条龙的网站。",
     },
     "/zh/": {
